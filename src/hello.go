1c698ddb1ca12443632055096f9fd86ce4845f8a
bino860 <haruo.banno@gmail.com>
2021-01-09 15:21:31 +0900

    変数の使い方

--- src/hello.go
+++ src/hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
+	var num int
+	num = 123
+	num1 := 456
 	fmt.Println("Hello,world!")
+	fmt.Println("num =", num)
+	fmt.Println("num1 =", num1)
 }
